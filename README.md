f37051829d2b2d870a7a439cbc43d6cfb7375229
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-10-18 08:51:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Python NESTED LOOP 2
+
+Python Nested Loop PDF:
+https://drive.google.com/file/d/1RLtJoZs6p5zrJi6IZ79tS-NTlhFuO4vw/view?usp=sharing
+
+
+---
+
+## Exercise 1
+
+**Problem:**
+
+Print below pattern with given n:
+
+**Example:**
+
+ 	Input: 5
+  	Output: 
+	1
+	2 2
+	3 3 3
+	4 4 4 4
+ 	5 5 5 5 5
+
+---
+
+## Exercise 2
+
+**Problem:**
+
+Print below pattern with given n:
+
+**Example**
+
+	Input: 5
+ 	Output:
+	    1 
+	   2 2 
+	  3 3 3 
+	 4 4 4 4 
+	5 5 5 5 5
+
+---
+
+## Exercise 3
+
+**Problem:**
+
+Print below pattern with given n:
+
+**Example**
+
+ 	Input: 5
+  	Output: 
+	1 
+	0 1
+	1 0 1 
+	0 1 0 1 
+	1 0 1 0 1
+
+---
---
+++ test_assignment.py
@@ -0,0 +1,43 @@
+import pytest
+import inspect
+from assignment import print_pattern_1, print_pattern_2, print_pattern_3
+
+def check_contains_loop(function):
+    source = inspect.getsource(function)
+    return 'for' in source or 'while' in source
+
+def test1(capsys):
+    expected_output = (
+        "1 \n"
+        "2 2 \n"
+        "3 3 3 \n"
+        "4 4 4 4 \n"
+        "5 5 5 5 5 \n"
+    )
+    print_pattern_1(5)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
+
+def test2(capsys):
+    expected_output = (
+        "    1 \n"
+        "   2 2 \n"
+        "  3 3 3 \n"
+        " 4 4 4 4 \n"
+        "5 5 5 5 5 \n"
+    )
+    print_pattern_2(5)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
+
+def test3(capsys):
+    expected_output = (
+        "1 \n"
+        "0 1 \n"
+        "1 0 1 \n"
+        "0 1 0 1 \n"
+        "1 0 1 0 1 \n"
+    )
+    print_pattern_3(5)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
